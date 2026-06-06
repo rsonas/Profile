0c6a33bc5879ce68f4960562a1ade8c9955d9982
rsonas <134102387+rsonas@users.noreply.github.com>
2026-06-06 19:38:59 -0400

    name fixes

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { createRoot } from 'react-dom/client'
-import './styles/app.css'
+import './styles/App.css'
 import App from './App.jsx'
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
